da1e95ce3cd22f1ebbe108f5dbfc738e51ba6ed3
Jdude311 <jadenjohn.ch@gmail.com>
2022-07-29 15:26:09 -0400

    Implemented type of object

--- game.h
+++ game.h
@@ -2,12 +2,16 @@
 /* LIBRARIES */
 #include <GL/glut.h> // for graphics
 #include <math.h> // for math
+#include <stdbool.h> // booleans, duh
 
 /* CONSTANTS */
 unsigned short game_width = 480;
 unsigned short game_height = 480;
 
+#define NUM_GAME_OBJECTS 255
+
 #define PI 3.141592654
+
 
 /* UTILITY FUNCTIONS */
 void placePixel (float x, float y, float r, float g, float b) {
@@ -28,24 +32,96 @@ void placePixel (float x, float y, float r, float g, float b) {
     glEnd();
 }
 
-/* DEFINE BASE OBJECTs */
-// Game Object
+/* DEFINE BASE OBJECTS */
+// Struct Definitions
 typedef struct GameObject GameObject;
 struct GameObject {
+    char type;
     float x;
     float y;
     float angle;
-    unsigned char velocity;
+    float velocity;
     unsigned char obj_index;
     unsigned char health;
     void (*move)(GameObject*); //self
     void (*draw)(GameObject*); // self
-    void (*draw_hitbox)(GameObject*, unsigned char*); // self
+    void (*draw_hitbox)(GameObject*, unsigned char*); // self, collision list--used internally
+    void (*check_collisions)(GameObject*); // self
     void (*collide)(GameObject*, GameObject*); // self, other obj
     void (*on_collide)(GameObject*); // self
 };
 
-GameObject createGameObject (float x,
+typedef struct Player Player;
+struct Player {
+    GameObject game_object;
+    int score;
+    void (*input)(Player *, bool*);
+};
+
+
+// Game (methods for full game and global game variables)
+struct Game {
+    // Global variables
+    GameObject** game_objects;
+    unsigned char* collision_list;
+    bool pressed_keys[255];
+    Player player;
+
+    // GL callback functions
+    void (*update)(void);
+    void (*display)(void);
+    void (*timer)(void);
+    void (*keyboard_up)(unsigned char key, int x, int y);
+    void (*keyboard_down)(unsigned char key, int x, int y);
+};
+struct Game game;
+
+void update () {
+    for (int i = 0; i < game_width * game_height; i++) {
+        game.collision_list[i] = 255;
+    }
+
+    for (int i = 0; i < NUM_GAME_OBJECTS; i++) {
+        game.game_objects[i] -> check_collisions(game.game_objects[i]);
+        game.game_objects[i] -> move(game.game_objects[i]);
+    }
+    game.player.input(&game.player, game.pressed_keys);
+}
+
+void display () {
+    // Prepare to draw
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // set background color
+    glClear(GL_COLOR_BUFFER_BIT); // clear color buffer
+
+    // Display game items
+    for (int i = 0; i < NUM_GAME_OBJECTS; i++) {
+        game.game_objects[i] -> draw(game.game_objects[i]);
+    }
+
+    // Finalize frame
+    glFlush(); // render
+}
+
+void keyboardUp (unsigned char key, int x, int y) {
+    game.pressed_keys[key] = false;
+    game.player.input(&game.player, game.pressed_keys);
+}
+
+void keyboardDown (unsigned char key, int x, int y) {
+    game.pressed_keys[key] = true;
+    game.player.input(&game.player, game.pressed_keys);
+}
+
+struct Game game = (struct Game) {
+    .update = update,
+    .display = display,
+    .keyboard_down = keyboardDown,
+    .keyboard_up = keyboardUp,
+};
+
+// Game Object
+GameObject createGameObject (char type,
+                             float x,
                              float y,
                              float angle,
                              char velocity,
@@ -56,6 +132,7 @@ GameObject createGameObject (float x,
                              void* collide,
                              void* on_collide) {
     GameObject temp = {
+        .type = type,
         .x = x,
         .y = y,
         .angle = angle,
@@ -78,21 +155,24 @@ void drawDummy (GameObject* self) {}
 
 void drawHitboxDummy (GameObject* self, unsigned char* collision_list) {}
 
+void checkCollisionsDummy (GameObject* self) {}
+
 void collideDummy (GameObject* self, GameObject* other) {}
 
 void onCollideDummy (GameObject* self) {}
 
 GameObject createDummy () {
     GameObject temp = {
+        .type = -1,
         .x = 3,
         .y = 0,
         .angle = 0,
         .velocity = 0,
-        .obj_index = 0,
+        .obj_index = 255,
         .health = 0,
         .move = &moveDummy,
         .draw = &drawDummy,
-        .draw_hitbox = &drawHitboxDummy,
+        .check_collisions = &checkCollisionsDummy,
         .collide = &collideDummy,
         .on_collide = &onCollideDummy,
     };
@@ -101,6 +181,7 @@ GameObject createDummy () {
 
 // Enemy Plane
 void moveEnemyPlane (GameObject* self) {
+    if (self -> health == 0) return;
     self -> angle += (((rand()%100)/100.0) - 0.5);
     self -> x += self -> velocity * cos(self -> angle * PI / 180);
     self -> y += self -> velocity * sin(self -> angle * PI / 180);
@@ -117,6 +198,7 @@ void moveEnemyPlane (GameObject* self) {
 }
 
 void drawEnemyPlane (GameObject* self) {
+    if (self -> health == 0) return;
     // Vertical E
     placePixel(self -> x  , self -> y  , 1.0, 0.0, 0.0);
     placePixel(self -> x  , self -> y-1, 1.0, 0.0, 0.0);
@@ -155,11 +237,23 @@ void drawEnemyPlane (GameObject* self) {
     placePixel(self -> x+7, self -> y-7, 1.0, 0.0, 0.0);
 }
 
-void drawHitboxEnemyPlane (GameObject* self, unsigned char* collision_list) {
-    // TODO implement
+void checkCollisionsEnemyPlane (GameObject* self) {
+    if (self -> health == 0) return;
+    bool check = true;
+    int i = 0;
     for (int xx = 0; xx < 8; xx++) {
         for (int yy = 0; yy < 8; yy++) {
-            collision_list[game_height * yy + xx] = self -> obj_index;
+            i = game_width * (self -> y + yy) + (self -> x + xx);
+            if (i < 0 || xx + self -> x > game_width || yy + self -> y > game_height) break;
+            if (check &&
+             game.game_objects[game.collision_list[i]] -> type != self -> type &&
+             game.game_objects[game.collision_list[i]] -> type >= 0 &&
+             game.collision_list[i] != self -> obj_index) {
+                printf("balls %f %f\n", self -> x, self -> y);
+                check = false;
+                /* self -> collide(self, game.game_objects[game.collision_list[i]]); */
+            }
+            game.collision_list[i] = self -> obj_index;
         }
     }
 }
@@ -171,7 +265,7 @@ void collideEnemyPlane (GameObject* self, GameObject* other) {
 }
 
 void onCollideEnemyPlane (GameObject* self) {
-    self->health--;
+    if (self -> health > 0) self -> health--;
     printf("EnemyPlane %d at health %d\n", self -> obj_index, self -> health);
     // TODO implement
  }
@@ -179,10 +273,11 @@ void onCollideEnemyPlane (GameObject* self) {
 GameObject createEnemyPlane (float x,
                              float y,
                              float angle,
-                             char velocity,
+                             float velocity,
                              unsigned char obj_index,
                              unsigned char health) {
     GameObject temp = {
+        .type = 1,
         .x = x,
         .y = y,
         .angle = angle,
@@ -191,7 +286,7 @@ GameObject createEnemyPlane (float x,
         .health = health,
         .move = &moveEnemyPlane,
         .draw = &drawEnemyPlane,
-        .draw_hitbox = &drawHitboxEnemyPlane,
+        .check_collisions = &checkCollisionsEnemyPlane,
         .collide = &collideEnemyPlane,
         .on_collide = &onCollideEnemyPlane,
     };
@@ -199,13 +294,6 @@ GameObject createEnemyPlane (float x,
 }
 
 // Player
-typedef struct Player Player;
-struct Player {
-    GameObject game_object;
-    int score;
-    void (*take_input)(Player *, int);
-};
-
 void movePlayer (GameObject* self) {
     self -> x += self -> velocity * cos(self -> angle * PI / 180);
     self -> y += self -> velocity * sin(self -> angle * PI / 180);
@@ -229,11 +317,20 @@ void drawPlayer (GameObject* self) {
     }
 }
 
-void drawHitboxPlayer (GameObject* self, unsigned char* collision_list) {
-    // TODO implement
+void checkCollisionsPlayer (GameObject* self) {
+    bool check = true;
+    int i = 0;
     for (int xx = 0; xx < 8; xx++) {
         for (int yy = 0; yy < 8; yy++) {
-            collision_list[game_height * yy + xx] = self -> obj_index;
+            i = game_width * (self -> y + yy) + (self -> x + xx);
+            if (i < 0 || xx + self -> x > game_width || yy + self -> y > game_height) break;
+            if (check &&
+             game.collision_list[i] != 255 &&
+             game.collision_list[i] != self -> obj_index) {
+                check = false;
+                self -> collide(self, game.game_objects[game.collision_list[i]]);
+            }
+            game.collision_list[i] = self -> obj_index;
         }
     }
 }
@@ -245,29 +342,31 @@ void collidePlayer (GameObject* self, GameObject* other) {
 }
 
 void onCollidePlayer (GameObject* self) {
-    self->health--;
-    printf("Player %d at health %d\n", self -> obj_index, self -> health);
- }
+    game.player.score++;
+    printf("Player %d score %d!!!\n", self -> obj_index, game.player.score);
+}
 
-void takeInputPlayer (Player* self, int key) {
-    // TODO implement
-    switch (key) {
-        case 97: // A
-            self -> game_object.angle += 10;
-            break;
-        case 100: // D
-            self -> game_object.angle -= 10;
-            break;
-        case 119: // W
-            if (self -> game_object.velocity < 10)
-                self -> game_object.velocity++;
-            break;
-        case 115: // S
-            if (self -> game_object.velocity > 0)
-                self -> game_object.velocity--;
-            break;
+void inputPlayer (Player* self, bool* pressed_keys) {
+    for (unsigned char key = 0; key < 255; key++) {
+        switch (key * pressed_keys[key]) {
+            case 97: // A
+                self -> game_object.angle += 5;
+                break;
+            case 100: // D
+                self -> game_object.angle -= 5;
+                break;
+            case 119: // W
+                self -> game_object.velocity += 0.1;
+                if (self -> game_object.velocity > 10)
+                    self -> game_object.velocity = 10;
+                break;
+            case 115: // S
+                self -> game_object.velocity -= 0.1;
+                if (self -> game_object.velocity < 0)
+                    self -> game_object.velocity = 0;
+                break;
+        }
     }
-
 }
 
 Player createPlayer (unsigned short x,
@@ -276,18 +375,20 @@ Player createPlayer (unsigned short x,
                      unsigned char health) {
     Player temp = {
         .game_object = {
+            .type = 0,
             .x = x,
             .y = y,
             .obj_index = obj_index,
             .health = health,
             .move = &movePlayer,
             .draw = &drawPlayer,
-            .draw_hitbox = &drawHitboxPlayer,
+            /* .draw_hitbox = &drawHitboxPlayer, */
+            .check_collisions = checkCollisionsPlayer,
             .collide = &collidePlayer,
             .on_collide = &onCollidePlayer,
         },
         .score = 0,
-        .take_input = &takeInputPlayer,
+        .input = &inputPlayer,
     };
     return temp;
 }
--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 /* INITIALIZATION FOR GLUT FUNCTIONS */
 // Initialize list for all GameObjects
-#define NUM_GAME_OBJECTS 255
+//#define NUM_GAME_OBJECTS 255
 GameObject** game_objects;
 // Initialize player
 Player player;
@@ -16,81 +16,77 @@ unsigned char* collision_list;
 
 /* FUNCTION DEFINITIONS */
 // Keyboard callback
-void keyboard_input (unsigned char key, int x, int y) {
-    printf("key %d\n", key);
-    player.take_input(&player, key);
-}
+/* void keyboard_input (unsigned char key, int x, int y) { */
+/*     player.input(&player, key); */
+/* } */
 
 // Update game objects
-void updateGameObjects (GameObject** game_objects, unsigned char len) {
-    for (int i = 0; i < len; i++) {
-        game_objects[i] -> draw_hitbox(game_objects[i], &collision_list);
-        game_objects[i] -> move(game_objects[i]);
-    }
-}
+/* void updateGameObjects (GameObject** game_objects, unsigned char len) { */
+/*     for (int i = 0; i < len; i++) { */
+/*         game.game_objects[i] -> draw_hitbox(game_objects[i], &collision_list); */
+/*         game.game_objects[i] -> move(game.game_objects[i]); */
+/*     } */
+/* } */
 
 // Idle function callback
-void update () {
-    updateGameObjects(game_objects, NUM_GAME_OBJECTS);
-}
+/* void update () { */
+/*     updateGameObjects(game_objects, NUM_GAME_OBJECTS); */
+/* } */
 
 // Display game objects
-void displayGameObjects (GameObject** game_objects, unsigned char len) {
-    // TODO implement opengl display callback
-    for (int i = 0; i < len; i++) {
-        game_objects[i] -> draw(game_objects[i]);
-    }
-}
+/* void displayGameObjects (GameObject** game_objects, unsigned char len) { */
+/* } */
 
 // GLUT timer function
 void timer () {
     glutTimerFunc(1000/60.0, timer, 0);
-    update();
+    game.update();
     glutPostRedisplay();
 }
 
 // GLUT display callback
-void display () {
-    // Prepare to draw
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // set background color
-    glClear(GL_COLOR_BUFFER_BIT); // clear color buffer
+/* void display () { */
+/*     // Prepare to draw */
+/*     glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // set background color */
+/*     glClear(GL_COLOR_BUFFER_BIT); // clear color buffer */
 
-    // Display game items
-    displayGameObjects(game_objects, NUM_GAME_OBJECTS);
+/*     // Display game items */
+/*     displayGameObjects(game.game_objects, NUM_GAME_OBJECTS); */
 
-    // Finalize frame
-    glFlush(); // render
-}
+/*     // Finalize frame */
+/*     glFlush(); // render */
+/* } */
 
 // Driver function
 int main (int argc, char** argv) {
     /* SETUP */
     // Initialize collision objects list
-    collision_list = malloc(sizeof(unsigned char) * game_width * game_height);
+    game.collision_list = malloc(sizeof(unsigned char) * game_width * game_height);
     for (int i = 0; i < game_width * game_height; i++) {
-        collision_list[i] = 255;
+        game.collision_list[i] = 255;
     }
 
     // Initialize game objects list
-    game_objects = malloc(sizeof(GameObject*) * NUM_GAME_OBJECTS+1);
+    game.game_objects = malloc(sizeof(GameObject*) * NUM_GAME_OBJECTS+1);
 
     // Fill game list with dummy object
     GameObject dummy = createDummy();
-    for (int i = 0; i < NUM_GAME_OBJECTS; i++) {
-        game_objects[i] = &dummy;
+    for (int i = 0; i <= NUM_GAME_OBJECTS; i++) {
+        game.game_objects[i] = &dummy;
     }
 
     // Initialize player
-    player = createPlayer(240, 240, 0, 10);
-    game_objects[0] = &(player.game_object);
+    game.player = createPlayer(240, 240, 0, 10);
+    game.game_objects[0] = &(game.player.game_object);
 
     // Initialize enemies
     unsigned char NUM_ENEMY_PLANES = 10;
     GameObject enemy_planes[NUM_ENEMY_PLANES];
     for (int i = 0; i < NUM_ENEMY_PLANES; i++) {
-        enemy_planes[i] = createEnemyPlane(240, 240, 0, 3, i+1, 10);
+        enemy_planes[i] = createEnemyPlane(10*i, 0, 15*i, 1, i+1, 1);
         // Put object pointers in game_objects list
-        game_objects[i+1] = &enemy_planes[i];
+        game.game_objects[i+1] = &enemy_planes[i];
+        game.game_objects[i+1] -> move(game.game_objects[i+1]);
     }
 
     // Graphics
@@ -99,13 +95,14 @@ int main (int argc, char** argv) {
     glutInitWindowSize(320, 320);
     glutInitWindowPosition(0,0);
     //glutIdleFunc(&update);
-    glutDisplayFunc(&display);
+    glutDisplayFunc(game.display);
     glutTimerFunc(1, timer, 0);
-    glutKeyboardFunc(keyboard_input);
+    glutSetKeyRepeat(GLUT_KEY_REPEAT_OFF);
+    glutKeyboardFunc(game.keyboard_down);
+    glutKeyboardUpFunc(game.keyboard_up);
+    /* glutKeyboardFunc(game.input); */
 
     /* TEST CLASSES */
-    enemy_planes[1].collide(&enemy_planes[1], &enemy_planes[2]);
-    player.game_object.collide(&(player.game_object), &enemy_planes[3]);
     glutMainLoop();
 
     return 0;
